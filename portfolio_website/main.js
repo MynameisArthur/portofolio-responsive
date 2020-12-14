0ae13f2eff6513f74f88a158db1b6363d42f58c6
Arthur <arthurd800@wp.pl>
2020-12-14 15:42:58 +0100

    added javascript and media queries, finished project

--- portfolio_website/main.js
+++ portfolio_website/main.js
@@ -1,1 +1,25 @@
-//JS
+const bodyElement = document.querySelector('body');
+const menuToggle = document.querySelector('.hamburger-menu');
+const heroElement = document.querySelector('.hero');
+const navList = document.querySelector('.nav-list');
+
+menuToggle.addEventListener('click', () => {
+    bodyElement.classList.toggle('is-open');
+    heroElement.classList.add('is-open');
+});
+
+navList.addEventListener('click', (e) => {
+    let clickedElement = e.target;
+    if (clickedElement.classList[0] === 'nav-link') {
+        bodyElement.classList.remove('is-open');
+    }
+});
+
+window.addEventListener('click', (e) => {
+    let clickedElement = e.target;
+    if (clickedElement.matches('.is-open')) {
+        [bodyElement, heroElement].forEach((elem) =>
+            elem.classList.remove('is-open')
+        );
+    }
+});
